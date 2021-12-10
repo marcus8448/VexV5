114578cab5be19585f8ae7c83142c8e6d6cdbb10
marcus8448 <marcus8448_git@outlook.com>
2021-12-09 17:45:54 -0800

    chore: tank drive

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,14 @@ void left(uint16_t distance, int32_t voltage);
 void stop();
 void move_for(uint16_t distance, int32_t rf, int32_t rb, int32_t lf, int32_t lb);
 void setState(ControlState state);
-lv_res_t on_button_click(struct _lv_obj_t * obj);
+lv_res_t on_force_autonomous_click(struct _lv_obj_t * obj);
 
-static Motor motor_rf(1);
-static Motor motor_rb(11);
-static Motor motor_lf(10);
-static Motor motor_lb(20);
-static Motor arm(2);
+static Motor motor_rf(10, true);
+static Motor motor_rb(20, true);
+static Motor motor_lf(1);
+static Motor motor_lb(11);
+static Motor arm(2, E_MOTOR_GEARSET_36);
+static Motor arm2(9, E_MOTOR_GEARSET_36, true);
 
 static Controller controller(E_CONTROLLER_MASTER);
 
@@ -27,40 +28,55 @@ static lv_obj_t* motor_rb_line = NULL;
 static lv_obj_t* motor_lf_line = NULL;
 static lv_obj_t* motor_lb_line = NULL;
 
-static bool force_auto = false;
+static bool force_autonomous = false;
 static ControlState controlState = INITIALIZE;
 
-lv_res_t on_button_click(struct _lv_obj_t * obj) {
-	force_auto = true;
-	lv_obj_set_click(force_auto_button, false);
+/**
+ * Called when the "Force Autonomous" button is pressed.
+ */
+lv_res_t on_force_autonomous_click(struct _lv_obj_t * obj) {
+	force_autonomous = true;
+	lv_obj_set_click(obj, false);
 	return LV_RES_OK;
 }
 
- void concurrent_screen_update(void* param) {
+/**
+ * Updates the screen at a rate of 10fps.
+ * Used for autoscroll because scrolling by hand doesn't work half the time.
+ */
+ void screen_update_task(void* param) {
 	while (true) {
 		update_screen();
-		delay(100);
+		delay(100); //10 updates per second as we don't care *that* much about the screen.
 	}
  }
 
- void print_motor_info() {
+/**
+ * Updates the motor debug info.
+ * Will print out errno values if an error occours.
+ */
+ void update_motor_info() {
 	set_line(motor_rf_line, "Right Front: " + std::to_string(print_error<int32_t>(motor_rf.get_voltage())));
 	set_line(motor_rb_line, "Right Back: "  + std::to_string(print_error<int32_t>(motor_rb.get_voltage())));
 	set_line(motor_lf_line, "Left Front: "  + std::to_string(print_error<int32_t>(motor_lf.get_voltage())));
 	set_line(motor_lb_line, "Left Back: "   + std::to_string(print_error<int32_t>(motor_lb.get_voltage())));
  }
 
+/**
+ * Called when the robot is first initialized.
+ */
 void initialize() {
-	init_screen(on_button_click);
+	init_screen(on_force_autonomous_click);
 	state_line = create_static_line("State: <uninitialized>");
 	motor_rf_line = create_static_line("RF volatage: <uninitialized>");
 	motor_rb_line = create_static_line("RB volatage: <uninitialized>");
 	motor_lf_line = create_static_line("LF volatage: <uninitialized>");
 	motor_lb_line = create_static_line("LB volatage: <uninitialized>");
-    Task task(concurrent_screen_update);
+    Task task(screen_update_task);
 
 	setState(ControlState::INITIALIZE);
 }
+
 
 void disabled() {
 	setState(ControlState::DISABLED);
@@ -68,69 +84,111 @@ void disabled() {
 
 void competition_initialize() {
 	setState(ControlState::COMPETITION_INITIALIZE);
-
 }
 
+/**
+ * Called when the robot is in it's autonomous state in a competition.
+ * Or when the "Force Autonomous" button is presssed on the screen.
+ */
 void autonomous() {
 	setState(ControlState::AUTONOMOUS);
 	
 	forwards(2000, 32);
 }
 
+/**
+ * Called when the robot is under driver control.
+ * Will delegate to autonomous control if the "Force Autonomous" button is pressed.
+ */
 void opcontrol() {
 	setState(ControlState::OPERATOR_CONTROL);
 
 	while (true) {
-		if (force_auto) {
+		if (force_autonomous) {
 			autonomous();
-			force_auto = false;
+			force_autonomous = false;
 			lv_obj_set_click(force_auto_button, true);
 
 			setState(ControlState::OPERATOR_CONTROL);
 		}
 
-		int joystickRotX = controller.get_analog(ANALOG_RIGHT_X);
-		int joystickY = controller.get_analog(ANALOG_LEFT_Y);
-		int joystickX = controller.get_analog(ANALOG_LEFT_X);
+		int joystickL = controller.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
+		int joystickR = controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y);
+		if (controller.get_digital(E_CONTROLLER_DIGITAL_R1)) {
+			arm.move(-127);
+			arm2.move(-127);
+		} else if (controller.get_digital(E_CONTROLLER_DIGITAL_R2)) {
+			arm.move(127);
+			arm2.move(127);
+		} else {
+			arm.move(0);
+			arm2.move(0);
+		}
 		
-		motor_rf.move(joystickY - joystickRotX + joystickX);
-		motor_rb.move(joystickY - joystickRotX + joystickX);
-		motor_lf.move(joystickY + joystickRotX - joystickX);
-		motor_lb.move(joystickY + joystickRotX - joystickX);
+		motor_rf.move(joystickR);
+		motor_rb.move(joystickR);
+		motor_lf.move(joystickL);
+		motor_lb.move(joystickL);
 
-		print_motor_info();
+		update_motor_info();
 
 		delay(20);
 	}
 }
 
+/**
+ * Moves the robot forwards in inches.
+ * @param distance The distance to move in inches.
+ */
 void forwards(uint16_t distance, int32_t voltage) {
-	move_for(distance, voltage, voltage, voltage, voltage);
+	move_for(distance / TIME_PER_INCH, voltage, voltage, voltage, voltage);
 }
 
+/**
+ * Moves the robot backwards in inches.
+ * @param distance The distance to move in inches.
+ */
 void backwards(uint16_t distance, int32_t voltage) {
-	move_for(distance, -voltage, -voltage, -voltage, -voltage);
+	move_for(distance / TIME_PER_INCH, -voltage, -voltage, -voltage, -voltage);
 }
 
-void right(uint16_t distance, int32_t voltage) {
-	move_for(distance, voltage, voltage, -voltage, -voltage);
+/**
+ * Turns the robot right.
+ * @param angle The distance to turn in degrees.
+ */
+void turn_right(uint16_t angle, int32_t voltage) {
+	move_for(angle, -voltage, -voltage, voltage, voltage);
 }
 
-void left(uint16_t distance, int32_t voltage) {
-	move_for(distance, -voltage, -voltage, voltage, voltage);
+/**
+ * Turns the robot left.
+ * @param angle The distance to turn in degrees.
+ */
+void turn_left(uint16_t angle, int32_t voltage) {
+	move_for(angle, -voltage, voltage, -voltage, -voltage);
 }
 
+/**
+ * Stops the robot.
+ */
 void stop() {
 	move_for(20, 0, 0, 0, 0);
 }
 
-void move_for(uint16_t distance, int32_t rf, int32_t rb, int32_t lf, int32_t lb) {
+/**
+ * Powers the motors for a specific amount of time.
+ * @param rf The power level for the motor on the front right.
+ * @param rb The power level for the motor on the back right.
+ * @param lf The power level for the motor on the front left.
+ * @param lb The power level for the motor on the back left.
+ */
+void move_for(uint16_t time, int32_t rf, int32_t rb, int32_t lf, int32_t lb) {
 	motor_rf = rf;
 	motor_rb = rb;
 	motor_lf = lf;
 	motor_lb = lb;
 
-	delay(distance / TIME_PER_INCH);
+	delay(time);
 
 	motor_rf = 0;
 	motor_rb = 0;
